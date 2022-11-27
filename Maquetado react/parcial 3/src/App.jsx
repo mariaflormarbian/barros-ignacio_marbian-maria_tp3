92f0e72ec25c5a2590f2e24f5620dc73a34e89c7
Florenica Marbian <maria.marbian@davinci.edu.ar>
2022-11-27 15:10:22 -0300

    Nuevo maquetado

---
+++ Maquetado react/parcial 3/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import './App.css'
+
+function App() {
+
+  return (
+   <NavBar/>
+
+  )
+}
+
+export default App
