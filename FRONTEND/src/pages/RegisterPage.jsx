a628f69566c0c1f1b15a8cc2e6ae5d22135bd2f5
Nacho1714 <102827516+Nacho1714@users.noreply.github.com>
2022-11-27 22:03:44 -0300

    correciones

--- FRONTEND/src/pages/RegisterPage.jsx
+++ FRONTEND/src/pages/RegisterPage.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 import * as RegisterService from "../services/register.services";
 
 function RegisterPage() {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
-  const [password, passwordChange] = useState("");
+  const [password, setPassword] = useState("");
 
   function emailChange(event) {
     setEmail(event.target.value);
+  }
+  
+  function passwordChange(event) {
+    setPassword(event.target.value);
   }
 
   function saveUser(event) {
@@ -22,27 +27,36 @@ function RegisterPage() {
     });
   }
   return (
+
     <section id="contact" className="full-height px-lg-5">
+
       <div className="container">
+
         <div className="row justify-content-center text-center">
+
           <div className="col-lg-8 pb-4">
             <h1>Registrarse</h1>
           </div>
 
           <div className="col-lg-8">
+
             <form onSubmit={saveUser} className="row g-lg-3 gy-3">
+
               <div className="form-group col-md-6">
+
                 <label className="visually-hidden" for="email">
                   Email
                 </label>
-                <input onChange={emailChange} value={email} />
+
+                <input className="form-control" placeholder="Email" onChange={emailChange} value={email} />
+
               </div>
 
               <div className="form-group col-md-6">
                 <label className="visually-hidden" for="link">
                   Contraseña
                 </label>
-                <input onChange={passwordChange} value={password} />
+                <input type="password" className="form-control" placeholder="Contraseña" onChange={passwordChange} value={password} />
               </div>
 
               <div className="form-group col-12 d-grid">
