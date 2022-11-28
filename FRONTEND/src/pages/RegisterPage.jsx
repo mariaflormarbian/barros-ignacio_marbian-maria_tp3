5f416752999a69b4c43fea259a0bbcf34093d27f
Florenica Marbian <maria.marbian@davinci.edu.ar>
2022-11-27 21:36:56 -0300

    registro final

--- FRONTEND/src/pages/RegisterPage.jsx
+++ FRONTEND/src/pages/RegisterPage.jsx
@@ -1,69 +1,61 @@
-import { useEffect, useState } from 'react'
-import * as RegisterService from '../services/register.services'
+import { useState } from "react";
+import * as RegisterService from "../services/register.services";
 
-function RegisterPage(){
-    const navigate = useNavigate()
+function RegisterPage() {
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('')
-    const [password, passwordChange] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, passwordChange] = useState("");
 
-    function emailChange(event){
-        setEmail(event.target.value)
-    }
+  function emailChange(event) {
+    setEmail(event.target.value);
+  }
 
-    function saveUser(event){
-        event.preventDefault()
+  function saveUser(event) {
+    event.preventDefault();
 
-        RegisterService.create({
-            email,
-            password
-        })
-        .then(function(){
-            navigate('/admin')
-        })
-    }
-    return (
-<section id="contact" className="full-height px-lg-5">
-
-<div className="container">
-
-    <div className="row justify-content-center text-center">
-        <div className="col-lg-8 pb-4">
+    RegisterService.create({
+      email,
+      password,
+    }).then(function () {
+      navigate("/admin");
+    });
+  }
+  return (
+    <section id="contact" className="full-height px-lg-5">
+      <div className="container">
+        <div className="row justify-content-center text-center">
+          <div className="col-lg-8 pb-4">
             <h1>Registrarse</h1>
-        </div>
+          </div>
 
-        <div className="col-lg-8">
-
+          <div className="col-lg-8">
             <form onSubmit={saveUser} className="row g-lg-3 gy-3">
+              <div className="form-group col-md-6">
+                <label className="visually-hidden" for="email">
+                  Email
+                </label>
+                <input onChange={emailChange} value={email} />
+              </div>
 
-                <div className="form-group col-md-6">
-                    <label className="visually-hidden" for="email">Email</label>
-                    <input 
-                        onChange={emailChange} value={email} / >
-                </div>
+              <div className="form-group col-md-6">
+                <label className="visually-hidden" for="link">
+                  Contraseña
+                </label>
+                <input onChange={passwordChange} value={password} />
+              </div>
 
-                <div className="form-group col-md-6">
-                    <label className="visually-hidden" for="link">Contraseña</label>
-                    <input 
-                        onChange={passwordChange} value={password}
-                   / >
-                </div>
-
-                <div className="form-group col-12 d-grid">
-                    <button type="submit" className="btn btn-brand">Registrar</button>
-                </div>
-
+              <div className="form-group col-12 d-grid">
+                <button type="submit" className="btn btn-brand">
+                  Registrar
+                </button>
+              </div>
             </form>
-
+          </div>
         </div>
-
-    </div>
-
-</div>
-
-</section>
-
-    )
+      </div>
+    </section>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
